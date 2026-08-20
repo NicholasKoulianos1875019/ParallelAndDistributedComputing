0fbbab39f8f3e44001f969d056973f8931468a16
Nicholas Koulianos <a1875019@adelaide.edu.au>
2026-08-20 11:07:53 +0930

    Ring formation created, untested

--- mpi_nbody_basic.c
+++ mpi_nbody_basic.c
@@ -149,8 +149,30 @@ int main(int argc, char* argv[]) {
       for (loc_part = 0; loc_part < loc_n; loc_part++)
          Update_part(loc_part, masses, loc_forces, loc_pos, loc_vel, 
                n, loc_n, delta_t);
-      MPI_Allgather(MPI_IN_PLACE, loc_n, vect_mpi_t, 
-                    pos, loc_n, vect_mpi_t, comm);
+      // Ring start
+      next = (my_rank + 1) % comm_sz;
+      previous = (my_rank - 1 + comm_sz) % comm_sz;
+      if (my_rank == 0) {
+        std::cout << "Process 0 starting the ring. Sending to Process " << next << "\n";
+        
+        // Process 0 sends first to break the blocking dependency chain
+        MPI_Send(loc_pos[0], loc_n, vect_mpi_t, next, 0, MPI_COMM_WORLD);
+        
+        // Process 0 blocks until the final process closes the ring loop
+        MPI_Recv(loc_pos[0], loc_n, vect_mpi_t, previous, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        std::cout << "Process 0 received token back from Process " 
+                  << previous << ". Ring complete!\n";
+      } else {
+        // All other processes wait to receive the token from their left neighbor
+        MPI_Recv(loc_pos[0], loc_n, vect_mpi_t, previous, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        
+        std::cout << "Process " << my_rank << " received token from Process " << previous << "\n";
+
+        // Send the token to the right neighbor
+        MPI_Send(loc_pos[0], loc_n, vect_mpi_t, next, 0, MPI_COMM_WORLD);
+    }
+      // MPI_Allgather(MPI_IN_PLACE, loc_n, vect_mpi_t, pos, loc_n, vect_mpi_t, comm);
+      // Ring end
 #     ifndef NO_OUTPUT
       if (step % output_freq == 0)
          Output_state(t, masses, pos, loc_vel, n, loc_n);
